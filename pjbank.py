203b2e4d2ee1c7da27988d28e1668f79c6faf24a
minhtridev2004 <129942243+minhtridev2004@users.noreply.github.com>
2023-06-17 19:29:02 +0700

    pjbank.py

--- pjbank.py
+++ pjbank.py
@@ -5,17 +5,21 @@ from PIL import ImageTk,Image
 #main screen 
 master = Tk()
 master.title('TN banking')
-master.configure(bg="#00BFFF")
+master.configure(bg="white")
+#var
+first_account_number =int( '0' * 2)
 #Function
 
 def finish_reg():
+
     name = temp_name.get()
     age = temp_age.get()
     gender = temp_gender.get()
     account = temp_account.get()
     password = temp_password.get()
+    num_account = temp_num_account.get()
     all_account = os.listdir()
-    if name == "" or age == "" or gender == "" or account == "" or password == "":
+    if name == "" or age == "" or gender == "" or account == "" or password == "" or num_account=="":
         notif.config(fg="red", text = "Vui lòng điền đầy đủ thông tin ")
         return
     for name_check in all_account:
@@ -28,6 +32,7 @@ def finish_reg():
     new_file.write(name+'\n')
     new_file.write(age+'\n')
     new_file.write(gender+'\n')
+    new_file.write(num_account+'\n')
     new_file.write('0')
     new_file.close()
     notif.config(fg="green", text ="Tài khoản được tạo thành công")
@@ -42,31 +47,36 @@ def register():
     global temp_password
     global notif
     global register_screen
+    global temp_num_account
     temp_name = StringVar()
     temp_age = StringVar()
     temp_gender = StringVar()
     temp_account = StringVar()
     temp_password = StringVar()
+    temp_num_account = StringVar()
     #screen
     register_screen = Toplevel(master)
     register_screen.title("Đăng kí")
+    register_screen.configure(bg="white")
     #label
-    Label(register_screen, text = "Nhập thông tin của bạn", font = ('Calibri, 12')).grid(row = 0, sticky = N, pady = 6)
-    Label(register_screen, text = "Họ và Tên", font = ('Calibri, 12')).grid(row = 1, sticky = W)
-    Label(register_screen, text = "Tuổi", font = ('Calibri, 12')).grid(row = 2, sticky = W)
-    Label(register_screen, text = "Giới tính", font = ('Calibri, 12')).grid(row = 3, sticky = W)
-    Label(register_screen, text = "Tên tài khoản", font = ('Calibri, 12')).grid(row = 4, sticky = W)
-    Label(register_screen, text = "Mật khẩu", font = ('Calibri, 12')).grid(row = 5, sticky = W)
-    notif = Label(register_screen,font = ('Calibri, 12'))
+    Label(register_screen, text = "Nhập thông tin của bạn", font = ('Calibri, 15'),bg="white").grid(row = 0,column=0, sticky = N, pady = 6)
+    Label(register_screen, text = "Họ và Tên", font = ('Calibri, 12'),bg="white").grid(row = 1, sticky = W)
+    Label(register_screen, text = "Tuổi", font = ('Calibri, 12'),bg="white").grid(row = 2, sticky = W)
+    Label(register_screen, text = "Giới tính", font = ('Calibri, 12'),bg="white").grid(row = 3, sticky = W)
+    Label(register_screen, text = "Tên tài khoản", font = ('Calibri, 12'),bg="white").grid(row = 4, sticky = W)
+    Label(register_screen, text = "Số điện thoại", font = ('Calibri, 12'),bg="white").grid(row = 5, sticky = W)
+    Label(register_screen, text = "Mật khẩu", font = ('Calibri, 12'),bg="white").grid(row = 6, sticky = W)
+    notif = Label(register_screen,font = ('Calibri, 12'),bg="white")
     notif.grid(row = 7, sticky = N, pady = 10)
     #entries
     Entry(register_screen,textvariable=temp_name).grid(row = 1, column=1)
     Entry(register_screen,textvariable=temp_age).grid(row = 2, column=1)
     Entry(register_screen,textvariable=temp_gender).grid(row = 3, column=1)
     Entry(register_screen,textvariable=temp_account).grid(row = 4, column=1)
-    Entry(register_screen,textvariable=temp_password,show='*').grid(row = 5, column=1)
+    Entry(register_screen,textvariable=temp_num_account).grid(row = 5, column=1)
+    Entry(register_screen,textvariable=temp_password,show='*').grid(row = 6, column=1)
     #buttons
-    Button(register_screen, text ="Đăng kí", command= finish_reg , font = ('Calibri', 12),bg="grey").grid(row = 6, stick = N, pady = 10)
+    Button(register_screen, text ="Đăng kí", command= finish_reg , font = ('Calibri', 12),bg="grey").grid(row = 7, stick = N, pady = 10)
 
 def infor():
     file = open(login_account , "r")
@@ -76,17 +86,20 @@ def infor():
     details_name = user_details[2]
     details_age = user_details[3]
     details_gender = user_details[4]
+    details_num_account = user_details[5]
     details_balance = user_details[-1]
 
     infor_screen = Toplevel(master)
     infor_screen.title("Thông tin cá nhân")
+    infor_screen.configure(bg="white")
     #labels
-    Label(infor_screen , text = "Thông tin cá nhân "+ details_name , font = ('Calibri', 12)).grid(row = 0, stick = N, pady = 10)
-    Label(infor_screen , text = "\tTên tài khoản: "+ details_account , font = ('Calibri', 12)).grid(row = 1, stick = W)
-    Label(infor_screen , text = "\tTên: "+ details_name , font = ('Calibri', 12)).grid(row = 2, stick = W)
-    Label(infor_screen , text = "\tTuổi: "+ details_age , font = ('Calibri', 12)).grid(row = 3, stick = W)
-    Label(infor_screen , text = "\tGiới Tính "+ details_gender , font = ('Calibri', 12)).grid(row = 4, stick = W)
-    Label(infor_screen , text = "\tSố dư: "+ details_balance , font = ('Calibri', 12)).grid(row = 5, stick = W)
+    Label(infor_screen , text = "Thông tin cá nhân "+ details_name , font = ('Calibri', 12),bg="white").grid(row = 0, stick = N, pady = 10)
+    Label(infor_screen , text = "\tSố tài khoản: "+ details_num_account , font = ('Calibri', 12),bg="white").grid(row = 1, stick = W)
+    Label(infor_screen , text = "\tTên tài khoản: "+ details_account , font = ('Calibri', 12),bg="white").grid(row = 2, stick = W)
+    Label(infor_screen , text = "\tTên: "+ details_name , font = ('Calibri', 12),bg="white").grid(row = 3, stick = W)
+    Label(infor_screen , text = "\tTuổi: "+ details_age , font = ('Calibri', 12),bg="white").grid(row = 4, stick = W)
+    Label(infor_screen , text = "\tGiới Tính "+ details_gender , font = ('Calibri', 12),bg="white").grid(row = 5, stick = W)
+    Label(infor_screen , text = "\tSố dư: "+ details_balance , font = ('Calibri', 12),bg="white").grid(row = 6, stick = W)
 
 
 def finish_deposit():
@@ -99,7 +112,7 @@ def finish_deposit():
     file = open(login_account, "r+")
     file_data = file.read()
     details = file_data.split('\n')
-    current_balance = details[5]
+    current_balance = details[6]
     update_balance = current_balance
     update_balance = float(update_balance) + float(amount.get())
     file_data = file_data.replace(current_balance, str(update_balance))
@@ -109,6 +122,101 @@ def finish_deposit():
     file.close()
     current_balance_label.config(text= "Số dư : $ "+ str(update_balance), fg="green")
     deposit_notif.config(text = "Nạp tiền thành công", fg = "green")
+
+def finish_transfer():
+    if transfer_amount.get() == "":
+        transfer_notif.config(text = "Vui lòng nhập số tiền ", fg = "red")
+        return
+    if float(transfer_amount.get()) <= 0 :
+        transfer_notif.config(text = "Giao dịch không thành công !", fg ="red")
+        return
+    global name_account
+    global login_account
+    global num_acc
+    # in4 user
+    file = open(login_account, "r+")
+    file_data = file.read()
+    all_account = os.listdir()
+    details = file_data.split('\n')
+    current_balance = details[6]
+    initial_balance = current_balance
+    #guest
+    name_account_guest = name_account.get()
+    file_guest = open(name_account_guest,"r+")
+    file_data_guest = file_guest.read()
+    all_account_guest = os.listdir()
+    details_guest = file_data_guest.split('\n')
+    current_balance_guest = details_guest[6]
+    check_num_account = details_account.get()
+    
+    print(current_balance)
+    update_balance_guest = current_balance_guest
+    update_balance = float(initial_balance) - float(transfer_amount.get())
+    print(update_balance)
+    update_balance_guest = float(update_balance_guest) + float(transfer_amount.get())
+    for acc in all_account_guest:
+        if acc == name_account_guest:
+            file = open(acc, "r+")
+            tmp_file = file_guest.read()
+            tmp_file = file_data_guest.split('\n')
+            num_acc = tmp_file[5]
+            print(num_acc)
+        if float(transfer_amount.get()) > float(current_balance):
+            transfer_notif.config(text = "Số dư không dủ", fg = "red")
+            return
+    if num_acc ==  check_num_account:
+        transfer_current_balance_label.config(text= "Số dư : $ "+ str(update_balance), fg="green")
+        transfer_notif.config(text = "Chuyển tiền thành công", fg = "green")
+        file_data = file_data.replace(str(initial_balance), str(update_balance))
+        file.seek(0)
+        file.truncate(0)
+        file.write(file_data)
+        file.close()
+
+        file_data_guest = file_data_guest.replace(current_balance_guest, str(update_balance_guest))
+        file_guest.seek(0)
+        file_guest.truncate(0)
+        file_guest.write(file_data_guest)
+        file_guest.close()
+        return
+    else:
+        transfer_notif.config(text ="Số tài khoản không hợp lệ")
+        return
+
+def transfer():
+    global name_account
+    global transfer_amount 
+    global transfer_notif
+    global  details_account
+    global transfer_current_balance_label
+    name_account = StringVar()
+    transfer_amount = StringVar()
+    file = open(login_account,"r")
+    file_data = file.read()
+    user_details = file_data.split('\n')
+    details_balance = user_details[6]
+    #stk
+    details_account = StringVar()
+    #deposit screen
+    transfer_screen = Toplevel(master)
+    transfer_screen.title("Chuyển tiền")
+    transfer_screen.configure(bg="lightgrey")
+    #label
+    Label(transfer_screen, text ="Chuyển tiền", font = ('calibri', 12)).grid(row = 0, sticky=N, pady = 10)
+    Label(transfer_screen, text ="Số tiền : ", font = ('Calibri', 12)).grid(row = 2 ,sticky=W)
+    Label(transfer_screen, text ="Số tài khoản : ", font = ('Calibri', 12)).grid(row = 4 ,sticky=W)
+    Label(transfer_screen, text ="Ten tài khoản : ", font = ('Calibri', 12)).grid(row = 3 ,sticky=W)
+    transfer_current_balance_label = Label(transfer_screen, text ="Số dư : $ " + details_balance, font=(('Calibri'), 12))
+    transfer_current_balance_label.grid(row = 1, sticky=W)
+    transfer_notif = Label(transfer_screen , font =('Calibri', 12))
+    transfer_notif.grid(row = 5, sticky=N , pady = 5)
+    #entry
+    Entry(transfer_screen, textvariable=transfer_amount).grid(row = 2, column=1)
+    Entry(transfer_screen, textvariable=name_account).grid(row = 3, column=1)
+    Entry(transfer_screen, textvariable=details_account).grid(row = 4, column=1)
+    #buttons
+    Button(transfer_screen, text = "Đồng ý", font =('Calibri', 12), command=finish_transfer,bg="grey").grid(row = 6, sticky=W, pady = 5)
+
 def deposit():
     global amount 
     global deposit_notif
@@ -121,6 +229,7 @@ def deposit():
     #deposit screen
     deposit_screen = Toplevel(master)
     deposit_screen.title("Nạp tiền")
+    deposit_screen.configure(bg="lightgrey")
     #label
     Label(deposit_screen, text ="Nạp tiền", font = ('calibri', 12)).grid(row = 0, sticky=N, pady = 10)
     current_balance_label = Label(deposit_screen, text ="Số dư : $ " + details_balance, font=(('Calibri'), 12))
@@ -143,7 +252,7 @@ def finish_withdraw():
     file = open(login_account, "r+")
     file_data = file.read()
     details = file_data.split('\n')
-    current_balance = details[5]
+    current_balance = details[6]
 
     if float(withdraw_amount.get()) > float(current_balance):
         withdraw_notif.config(text ="Số dư không đủ", fg="red")
@@ -189,7 +298,6 @@ def login_session():
     login_password = temp_login_password.get()
     for acc in all_account:
         if acc == login_account:
-            print("dang nhap thanh cong")
             file = open(acc, "r")
             file_data = file.read()
             file_data = file_data.split('\n')
@@ -199,6 +307,7 @@ def login_session():
                 login_screeen.destroy()
                 account_dashboard = Toplevel(master)
                 account_dashboard.title('TN banking')
+                
                 #labels
                 Label(account_dashboard, text = "Trang chủ" , font = ('Calibri', 12)).grid(row = 0, stick = N, pady = 10)
                 Label(account_dashboard, text = "Xin chào"+ acc , font = ('Calibri', 12)).grid(row = 1, stick = N, pady = 10)
@@ -206,6 +315,7 @@ def login_session():
                 Button(account_dashboard, text = "Thông tin cá nhân",font = ('Calibri', 12), width = 25, command = infor, bg="grey" ).grid(row = 2, stick = N, padx = 5)
                 Button(account_dashboard, text = "Nạp tiền",font = ('Calibri', 12), width = 25, command=deposit, bg="grey").grid(row = 3, stick = N, padx = 5)
                 Button(account_dashboard, text = "Rút tiền",font = ('Calibri', 12), width = 25, command=withdraw, bg="grey").grid(row = 4, stick = N, padx = 5)
+                Button(account_dashboard, text = "Chuyển tiền",font = ('Calibri', 12), width = 25, command= transfer, bg="grey").grid(row = 5, stick = N, padx = 5)
                 Label(account_dashboard).grid(row = 5, sticky=N,pady = 10)
                 return 
             else:
@@ -223,10 +333,11 @@ def login():
     temp_login_password = StringVar()
     login_screeen = Toplevel(master)
     login_screeen.title("Đăng nhập")
+    login_screeen.configure(bg="white")
     # label
-    Label(login_screeen, text = "Đăng nhập bằng tài khoản của bạn", font = ('Calibri', 12)).grid(row = 0, sticky= N,pady = 6)
-    Label(login_screeen, text = "Tên tài khoản", font = ('Calibri', 12)).grid(row = 1, sticky= W)
-    Label(login_screeen, text = "Mật khẩu", font = ('Calibri', 12)).grid(row = 2, sticky= W)
+    Label(login_screeen, text = "Đăng nhập bằng tài khoản của bạn", font = ('Calibri', 12), bg="white").grid(row = 0, sticky= N,pady = 6)
+    Label(login_screeen, text = "Tên tài khoản", font = ('Calibri', 12),bg="white").grid(row = 1, sticky= W)
+    Label(login_screeen, text = "Mật khẩu", font = ('Calibri', 12),bg="white").grid(row = 2, sticky= W)
     login_notif = Label(login_screeen, font =('Calibri', 12))
     login_notif.grid(row = 4,sticky=N)
     # entried
@@ -236,7 +347,7 @@ def login():
     Button(login_screeen, text ="Đăng nhập", command= login_session, width= 15 , font = ('Calibri', 12), bg="grey").grid(row = 3, stick = N, pady = 5, padx= 5)
 #image
 img = Image.open('project_python\image.jpg')
-img = img.resize((150,150))
+img = img.resize((250,250))
 img =  ImageTk.PhotoImage(img)
 #lable
 Label(master, text="TN banking", font =('',14), width = 40).grid(row=0, sticky=N, pady=10)
@@ -246,4 +357,4 @@ Label(master, image= img).grid(row = 2, sticky = N, pady =  15)
 Button(master, text ='Đăng kí', font = ('Calibri', 12), width=20 , command=register, bg="grey").grid(row = 3, sticky=N)
 Button(master, text ='Đăng nhập', font = ('Calibri', 12), width=20, command=login, bg="grey").grid(row = 4, sticky=N, pady = 6)
 
-master.mainloop()
+master.mainloop()       
